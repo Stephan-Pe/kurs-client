07be604e6bddd6c88039885e934e0e3ece244d30
Stephan-Pe <st.petersen63@gmail.com>
2022-06-27 22:34:57 +0200

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="loadData">Daten Laden</button>
+    <hr>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import { axios } from "axios";
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      posts: [
+        {id: 1, title: 'Test'}
+      ]
+    }
+  },
+  methods:{
+    loadData: function () {
+      
+    }
   }
 }
 </script>
